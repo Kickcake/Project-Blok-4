283af63544d9c418fb41543c5cdbf42daa72f82c
Kickcake <Kickvdstruik@gmail.com>
2023-06-07 13:55:21 +0200

    gebruiker toevoegen/registreren en adres toevoegen

---
+++ site/verwerk-registreren.php
@@ -0,0 +1,19 @@
+<?php
+require 'database.php';
+
+if (!empty($_POST['vnaamg'])) {
+    $Vnaam = $_POST['vnaamg'];
+    $tussen = $_POST['tusseng'];
+    $Anaam = $_POST['anaamg'];
+    $geslacht = $_POST['geslachtg'];
+    $mobiel = $_POST['mobielg'];
+    $email = $_POST['emailg'];
+    $GBnaam = $_POST['gbnaamg'];
+    $paswoord = $_POST['paswoordg'];
+
+    $sql = "INSERT INTO Gebruiker (voornaam, tussenvoegsel, achternaam, geslacht, mobielnummer, email, gebruikersnaam, paswoord) 
+                            VALUES ('$Vnaam', '$tussen', '$Anaam', '$geslacht', '$mobiel', '$email', '$GBnaam', '$paswoord')";
+    if (mysqli_query($conn, $sql)) {
+        header("location: adres_toevoegen.php");
+    };
+}
